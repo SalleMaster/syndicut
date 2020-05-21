d4e07dfa036ac95d220d06b09e6301d4eb0a3416
Sasa Radovanovic <salle90ar@gmail.com>
2020-05-21 14:49:43 +0200

    Main footer started

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,13 @@
 const navbarMenuToggle = document.querySelector('.navbar-menu-toggle');
 const navbarLinks = document.querySelector('.navbar-links');
 const navbarBrand = document.querySelector('.navbar-brand');
+const html = document.querySelector('html');
 
 const toggleNavbar = () => {
   navbarMenuToggle.classList.toggle('active');
   navbarLinks.classList.toggle('active');
   navbarBrand.classList.toggle('active');
+  html.classList.toggle('nav-active');
 };
 
 navbarMenuToggle.addEventListener('click', toggleNavbar);
